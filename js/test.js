c3c72793f5992f59b5e1514e5a194193f50101ba
Ryan Lau <hhrl2@ad.cl.cam.ac.uk>
2016-08-05 15:59:40 +0100

    Faked login page with javascript.

---
+++ js/test.js
@@ -0,0 +1,4 @@
+function myFunction()
+{
+   document.getElementById("demo").innerHTML = "Sorry, this doesn't actually work.<br /> Btw your password is " + document.getElementById("inputPassword").value + ".";
+}
